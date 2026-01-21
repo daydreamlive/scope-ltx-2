782cc1228b3fa076fbd8e2143e531e7a1556553a
BuffMcBigHuge <marco@bymar.co>
2026-01-21 18:15:47 -0500

    Fix to plugin registration.
    Signed-off-by: BuffMcBigHuge <marco@bymar.co>

--- scope_ltx_2/__init__.py
+++ scope_ltx_2/__init__.py
@@ -1,6 +1,15 @@
-"""LTX2 text-to-video pipeline."""
+"""LTX-2 text-to-video pipeline plugin for Daydream Scope."""
+
+import scope.core
 
 from .pipeline import LTX2Pipeline
 from .schema import LTX2Config
+
+
+@scope.core.hookimpl
+def register_pipelines(register):
+    """Register the LTX2 pipeline with Scope."""
+    register(LTX2Pipeline)
+
 
 __all__ = ["LTX2Pipeline", "LTX2Config"]
